7250c2405c8d99c84409d55a670d2fe66e370aeb
nwachiii <egreatness1@gmail.com>
2022-05-15 11:16:13 +0100

    clean up

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import { ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { ColorModeScript } from '@chakra-ui/react';
+
 import App from './App';
 
 const container = document.getElementById('root');
