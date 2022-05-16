381ed39fa07d721df298a2a771e125b83990883d
nwachiii <egreatness1@gmail.com>
2022-05-16 01:50:00 +0100

    dashboard (w.i.p)

--- src/components/Tables/Capsule.js
+++ src/components/Tables/Capsule.js
@@ -1,37 +1,37 @@
-import { Flex } from '@chakra-ui/react';
-import React from 'react'
+import {Flex} from '@chakra-ui/react';
+import React from 'react';
 
-export const Capsule = ({ data }) => {
-    let color = null;
+export const Capsule = ({data}) => {
+	let color = null;
 
-    switch (data) {
-        case 'Processing':
-            color = 'yellow';
-            break;
-        case 'Pending':
-            color = 'black';
-            break;
-        case 'Cancelled':
-            color = 'red';
-            break
-        case 'Paid':
-            color = 'green';
-            break;
-        default:
-            break;
-    }
+	switch (data) {
+		case 'Processing':
+			color = 'yellow';
+			break;
+		case 'Pending':
+			color = 'black';
+			break;
+		case 'Cancelled':
+			color = 'red';
+			break;
+		case 'Paid':
+			color = 'green';
+			break;
+		default:
+			break;
+	}
 
-    return (
-        <Flex
-            borderRadius='20px'
-            color={color}
-            textAlign='center'
-            borderColor={color}
-            py={2}
-            px={3}
-            width='fit-content'
-            fontSize='14px'>
-            {data}
-        </Flex>
-    );
-}
+	return (
+		<Flex
+			color={color}
+			py={2}
+			px={3}
+			borderColor={color}
+			borderRadius='20px'
+			textAlign='center'
+			width='fit-content'
+			fontSize='14px'>
+			{data}
+		</Flex>
+	);
+};
